ca89606ffff73c195d598a7b4728ba52c18d00f6
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-03-20 08:58:15 +0100

    izniburak router added, bramus router upd

--- src/routers/IzniburakRouter/IzniburakRouter.php
+++ src/routers/IzniburakRouter/IzniburakRouter.php
@@ -2,18 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Rehor\Myblog\routers\BramusRouter;
+namespace Rehor\Myblog\routers\IzniburakRouter;
 
 use Rehor\Myblog\routers\Router\Router;
 use Rehor\Myblog\routers\Router\traits\RouterTrait;
+use Buki\Router\Router as IzniRouter;
 
-class BramusRouter extends Router
+class IzniburakRouter extends Router
 {
     use RouterTrait;
 
     public function __construct()
     {
-        $this->router = new \Bramus\Router\Router();
+        $this->router = new IzniRouter();
     }
     
     public function __destruct()
@@ -24,16 +25,15 @@ class BramusRouter extends Router
     public function useGet(string $path, ?callable $fn): void
     {
         $this->router->get($path, $this->hasCallable(fn() => function() use($path) {
-            echo "Got $path alternatively from BramusRouter";
-        }, $fn));
+            echo "Got $path from IzniburakRouter";
+        }), $fn);
         $this->router->run();
     }
     
     public function usePost(string $path, ?callable $fn): void
     {
         $this->router->post($path, $this->hasCallable(fn() => function() use($path) {
-            echo "Posted $path alternatively from BramusRouter";
-        }, $fn));
-        $this->router->run();
+            echo "Posted $path from IzniburakRouter";
+        }), $fn);
     }
 }
